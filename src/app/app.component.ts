8e03ce83616888172bf992a611a6685a3027f639
Zach Marbach <Zach.Marbach@improving.com>
2019-12-03 21:06:06 -0600

    implemented Angular number pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NG-madLib';
+  
+  noun: string = "";
+  color: string = "";
+  bodyPart: string = "";
+  number: number = null;
+  isShown: boolean = false;
+
+  show() {
+    this.isShown = true;
+  }
+
+  reset(){
+    this.noun = "",
+    this.color =  "",
+    this.bodyPart = "";
+    this.number = null;
+    this.isShown = false;
+  }
+
 }
